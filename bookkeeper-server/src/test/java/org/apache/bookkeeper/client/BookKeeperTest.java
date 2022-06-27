0546e2547448cf9a51cd5ec1bec44418d2199aef
AndreaPepe <pepe.andmj@gmail.com>
2022-06-27 09:24:51 +0200

    Test not running on windows

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperTest.java
@@ -2,9 +2,12 @@ package org.apache.bookkeeper.client;
 
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.util.LocalBookKeeper;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
 
 public class BookKeeperTest {
 
@@ -12,19 +15,27 @@ public class BookKeeperTest {
     private BookKeeper bkc;
 
 
-//    @Before
-//    public void setupServer() throws Exception {
-//        ServerConfiguration conf = new ServerConfiguration();
-//        conf.setAllowLoopback(true);
-//        this.localBk = LocalBookKeeper.getLocalBookies("127.0.0.1", 5555, 3, true, conf);
-//        bkc = new BookKeeper("127.0.0.1:2181");
-//    }
-
-    @Test
-    public void dummyTest() throws Exception {
+    @Before
+    public void setupServer() throws Exception {
         ServerConfiguration conf = new ServerConfiguration();
         conf.setAllowLoopback(true);
-        this.localBk = LocalBookKeeper.getLocalBookies("127.0.0.1", 5555, 3, true, conf);
+        this.localBk = LocalBookKeeper.getLocalBookies("127.0.0.1", 2181, 3, true, conf);
+        this.localBk.start();
+        bkc = new BookKeeper("127.0.0.1:2181");
+    }
+
+    @Test
+    public void dummyTest() throws BKException, InterruptedException {
+        byte[] passwd = "passwd".getBytes(StandardCharsets.UTF_8);
+        LedgerHandle handle = bkc.createLedger(BookKeeper.DigestType.DUMMY, passwd);
+        long numBookies = handle.getNumBookies();
         Assert.assertTrue(true);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        bkc.close();
+        localBk.shutdownBookies();
+        localBk.close();
     }
 }
